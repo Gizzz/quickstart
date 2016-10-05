54c3814ab48a4bad2327fa535831ff264b6f5af5
Gizzz <bulatkhusainov@yandex.ru>
2016-10-05 21:48:55 +0300

    move "hero-detail" and "hero-search" component's parts in separate folders

--- app/app.module.ts
+++ app/app.module.ts
@@ -11,9 +11,9 @@ import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppComponent }  from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
-import { HeroDetailComponent } from "./hero-detail.component";
+import { HeroDetailComponent } from "./hero-detail/hero-detail.component";
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { HeroSearchComponent } from './hero-search.component';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 import { HeroService } from "./hero.service";
 import { routing } from './app.routing';
 
